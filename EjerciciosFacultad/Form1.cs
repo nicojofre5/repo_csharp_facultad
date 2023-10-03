218e07e507af3d0eb16812f2b245f7f2eed5c9d7
Nicolas <lnicolas.jofre@outlook.com>
2023-10-04 01:49:28 +0200

    Comit inicial

---
+++ EjerciciosFacultad/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjerciciosFacultad
+{
+    public partial class Form1 : Form
+    {
+        internal persona personaObj;
+        public Form1()
+        {
+            InitializeComponent();
+            personaObj= new persona();  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            personaObj.nombre = textBox1.Text;
+            personaObj.apellido = textBox2.Text;
+            MessageBox.Show("Ha sido asignado el nombre y apellido");
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(personaObj.mostrarNombre());
+        }
+    }
+}
---
+++ EjerciciosFacultad/persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace EjerciciosFacultad
+{
+    internal class persona
+    {
+
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+
+        public string mostrarNombre()
+        {
+            return "El nombre es: " + nombre + "\r\nEl apellido es:" + apellido;
+        }
+    }
+}
