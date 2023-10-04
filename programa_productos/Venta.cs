2f6d4a75768d58a0205cdaf90154716e287b77f7
Nicolas <lnicolas.jofre@outlook.com>
2023-10-04 02:59:51 +0200

    Clases creadas

---
+++ programa_productos/Venta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_productos
+{
+    public class Venta
+    {
+        private float subtotal;
+        private int id;
+        private DateTime fecha = DateTime.Now;
+        public float Subtotal
+        {
+            get { return subtotal; }
+            set { subtotal = value; }
+        }
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+        }
+
+        public void AgregarProducto(int cantidad, Producto objProducto)
+        {
+            subtotal+= (cantidad * objProducto.Precio);
+        }
+    }
+}
---
+++ programa_productos/gestorlibreria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_productos
+{
+    public class gestorlibreria
+    {
+        public Venta ventaActual;
+        private float totalRecaudado;
+
+        public float Getrecaudacion()
+        {
+            return totalRecaudado;
+        }
+        public bool IniciarVenta()
+        {
+            if(ventaActual == null)
+            {
+                ventaActual = new Venta(); //genero instancia el objeto venta
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void FinalizarVenta()
+        {
+            if (ventaActual != null) 
+            {
+                totalRecaudado += ventaActual.Subtotal;
+                ventaActual = null;
+            }
+        }
+
+    }
+}
