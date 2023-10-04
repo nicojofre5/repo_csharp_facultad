9f722c287c1d871f891148fff9b65f4272418c72
Nicolas <lnicolas.jofre@outlook.com>
2023-10-04 02:34:47 +0200

    Arreglo clase

---
+++ programa_productos/Producto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_productos
+{
+    public class Producto
+    {
+        private int id;
+        private int codigo;
+        private int nombre;
+        public string descripcion;
+        private float precio;
+
+        public int Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public float Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+
+
+
+    }
+}
